def99186565691828d141e45843e7bbc8f8e3a73
enea <enea.tsanai@gmail.com>
2015-09-11 02:28:33 -0400

    Updated Controller class.

--- src/AllJumbledUp/AllJumbledUp.java
+++ src/AllJumbledUp/AllJumbledUp.java
@@ -1,6 +1,8 @@
 package AllJumbledUp;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -8,14 +10,30 @@ import javafx.stage.Stage;
 
 public class AllJumbledUp extends Application {
 
+    private ObservableList<JumbledWord> JumbledWords = FXCollections.observableArrayList();
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("AllJumbledUp.fxml"));
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = loader.load(getClass().getResource("AllJumbledUp.fxml"));
+
+        // Give the controller access to the main app.
+        Controller controller = loader.getController();
+        controller.setMainApp(this);
+
         primaryStage.setTitle("All Jumbled Up");
         primaryStage.setScene(new Scene(root, 600, 465));
         primaryStage.show();
     }
 
+    public void genJumbledWords() {
+        JumbledWords.add(new JumbledWord("TestA"));
+        JumbledWords.add(new JumbledWord("TestB"));
+    }
+
+    public ObservableList<JumbledWord> getJumbledWords() {
+        return JumbledWords;
+    }
 
     public static void main(String[] args) {
         launch(args);
--- src/AllJumbledUp/Controller.java
+++ src/AllJumbledUp/Controller.java
@@ -1,4 +1,49 @@
 package AllJumbledUp;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class Controller {
+    @FXML
+    private TableView<JumbledWord> JumbledWordsA1;
+    @FXML
+    private TableColumn<JumbledWord, String> JumbledColumA1;
+    @FXML
+    private TableColumn<JumbledWord, String> GuessColumnA1;
+
+    // Reference to the main application.
+    private AllJumbledUp allJumbledUp;
+
+
+    /**
+     * The constructor.
+     * The constructor is called before the initialize() method.
+     */
+    public Controller() {
+    }
+
+
+    /**
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+    @FXML
+    private void initialize() {
+        // Initialize the person table with the two columns.
+        JumbledColumA1.setCellValueFactory(cellData -> cellData.getValue().getJumbledWordProperty());
+        GuessColumnA1.setCellValueFactory(cellData -> cellData.getValue().getWordProperty());
+    }
+
+    /**
+     * Is called by the main application to give a reference back to itself.
+     *
+     * @param allJumbledUpApp
+     */
+    public void setMainApp(AllJumbledUp allJumbledUpApp) {
+        this.allJumbledUp = allJumbledUpApp;
+
+        // Add observable list data to the table
+        JumbledWordsA1.setItems(allJumbledUp.getJumbledWords());
+    }
 }
