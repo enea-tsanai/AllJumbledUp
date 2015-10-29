a3417e637273a9c24a605985b6c28fe9c62514e1
enea tsanai <enea.tsanai@gmail.com>
2015-10-29 16:11:57 -0400

    Added general testing functions for Database

--- src/test/java/Database/DbQueries.java
+++ src/test/java/Database/DbQueries.java
@@ -4,4 +4,16 @@ package Database;
  * Created by enea on 10/29/15.
  */
 public class DbQueries {
+
+    //TODO: Check connection
+    public void dbConnectionSuccesful (){
+    }
+
+    //TODO: Check collections
+    public void dbCollectionsSuccesful (){
+    }
+
+    //TODO: Check error handling
+    public void dbCollectionFailureHandling (){
+    }
 }
